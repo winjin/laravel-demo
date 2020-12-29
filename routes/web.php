44e9282565cef23d7a6005f71363e80afb1f5ab6
winjin <84252802@qq.com>
2020-12-29 20:25:43 +0800

    set app and web page

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,17 @@
 
 Route::get('/', function () {
     return view('welcome');
+    // return view('back/login');
 });
+
+Route::get('login', 'Back\LoginController@index');
+Route::post('login/login', 'Back\LoginController@login');
+Route::get('users', 'Back\UserController@index');
+
+// Http\Controllers\Back\LoginController.php
+
+// Route::get('/home', 'HomeController@index');
+// Http\Controllers\HomeController.php
+
+// Route::post('changePic','Members\MembersController@changePic');
+// D:\laragon\www\dcr_server_php\app\Http\Controllers\Members\MembersController.php
